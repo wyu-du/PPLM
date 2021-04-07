0e382cf804abd92003a5cb2e994a1d5ac6c42acf
wyu-du <duwanyu0706@163.com>
2021-04-07 15:58:52 +0800

    update generate

---
+++ generate.sh
@@ -0,0 +1,16 @@
+
+export device_id=1
+
+CUDA_VISIBLE_DEVICES=$device_id python run_pplm.py \
+-D generic \
+--discrim_weights dailydialog_discriminator/generic_classifier_head_epoch_10.pt \
+--discrim_meta dailydialog_discriminator/generic_classifier_head_meta.json \
+--length 50 \
+--gamma 1.0 \
+--num_iterations 10 \
+--num_samples 10 \
+--stepsize 0.04 \
+--kl_scale 0.01 \
+--gm_scale 0.95 \
+--sample
+
