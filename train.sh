5df8fdef354acaad19bcd3197257e50efcf3a8cb
wyu-du <duwanyu0706@163.com>
2021-04-11 21:09:57 +0800

    update

--- train.sh
+++ train.sh
@@ -3,6 +3,7 @@ export device_id=1
 
 CUDA_VISIBLE_DEVICES=$device_id python run_pplm_discrim_train.py \
   --dataset generic \
+  --pretrained_model microsoft/DialoGPT-medium \
   --dataset_fp data/dailydialog_dis_train.txt \
   --output_fp dailydialog_discriminator \
   --save_model
